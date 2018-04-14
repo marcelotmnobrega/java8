5bdeaeb34f35b3f2d2ef035c2e1ec168ef07c30c
mtmn <marcelotmnobrega@gmail.com>
2018-04-14 11:17:08 -0300

    Adding some Stream calls.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,24 @@
 import java.io.File;
 import java.io.FileFilter;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class App {
 
     public static void main (String[] args){
+        Stream<String> stream = Stream.of("One", "Two", "Three", "Four", "Five");
+        stream
+                .filter(s -> s.length() > 3)
+                .forEach(System.out::println);
+
+        //Optional<String> one = stream.findAny();
+        //System.out.println(one.isPresent()?one.get():"null");
+        //stream.forEach(System.out::println);
+
+    }
+
+    public static void main3 (String[] args){
         FileFilter filterLambda = (File pathname) -> pathname.getName().endsWith(".java");
         File dir = new File("D:\\mtmn\\OneDrive\\Documents\\dev\\java\\java8\\src\\main\\java");
         File[] files = dir.listFiles(filterLambda);
